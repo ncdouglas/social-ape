5717942061d3d5c1133a3badf2b0dc3e70765633
Natasha Douglas <ncdouglas2016@gmail.com>
2021-06-08 11:57:57 -0500

    first commit

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user', 
+            body: 'scream body',
+            createdAt: '2021-06-08T16:45:57.129Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
